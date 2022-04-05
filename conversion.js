e64be2b4da459ae584f031fddc40b1df20544dee
Daniel Tucker <90122022+dantuckcse@users.noreply.github.com>
2022-04-04 21:45:29 -0700

    Add files via upload

--- conversion.js
+++ conversion.js
@@ -1,28 +1,52 @@
+const csv = require('csv-parser');
+const fs = require('fs');
 const express = require('express');
+
+const results = [];
+var number1; 
+var number2;
+
+const ps = require("prompt-sync");
+const prompt = ps();
+
+let currency1 = prompt("Enter your desired currency: ")
+let currency2 = prompt("Enter the currency you want to convert: ");
+var amount = prompt("Enter the amount of currency you want to convert: ");
+
+fs.createReadStream('Currency Database.csv')
+  .pipe(csv())
+  .on('data', (data) => {
+    if (data['Currency_Type'] === currency1) {
+      results.push(data);
+    }
+  })
+  .on('end', () => {
+    global.number1 = results[0].USD_Rate;
+  })
+
+
+  fs.createReadStream('Currency Database.csv')
+  .pipe(csv())
+  .on('data', (data) => {
+    if (data['Currency_Type'] === currency2) {
+      results.push(data);
+    }
+  })
+  .on('end', () => {
+    global.number2 = results[1].USD_Rate;
+  })
 
 const app = express();
 
-app.listen(5000, function() {
-  console.log(`Server is up and running on 5000 ...`);
-});
-
 app.get("/", function(req, res) {
 
-  //desired currency (Euro)
-  //needs to be modified to equal data from csv file 
-  var currency1 = 0.917154;
+  var result = (amount * global.number1) / global.number2;
 
-  //currency to converted (Pound)
-  //needs to be modified to equal data from csv file 
-  var currency2 = 0.763315; 
-
-  //amount of currency to be converted
-  //needs to be modified to equal user input
-  var amount = 385.56;
-
-  //conversion equation (pound --> euro)
-  var result = (amount * currency1) / currency2;
   result = Math.floor(result * 100) / 100
 
   res.send(result + "");
+});
+
+app.listen(5000, function() {
+  console.log(`Server is up and running on 5000 ...`);
 });

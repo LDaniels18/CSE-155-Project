3a509ef4f1bb9da6960586f6a7142607ff3d49ed
LDaniels18 <lafrancedanielsiii@hotmail.com>
2022-04-18 12:50:29 -0700

    Fresh state before conversion is added

--- conversion.js
+++ conversion.js
@@ -1,38 +1,40 @@
-//--External Javascript file to handle currency Conversions--
+const csv = require('csv-parser');
+const fs = require('fs');
+const express = require('express');
 
-//packages - const = constant refernce to a varaiable (so use when making functions)
-const csv = require('csv-parser'); //package to help with the parsing(formating and reading) of csv data
-const fs = require('fs'); //file system module -- package
-const express = require('express'); //a popular package that helps with web framework
-const PORT = 9999; //a contant port variable
-
-//variables
-const results = []; //an array that is referenced and can be altered by adding or deleting specific values
-var number1;
+//do not change
+const results = [];
+var number1; 
 var number2;
 
-const ps = require("prompt-sync"); //js package
+//used to take a user input
+//can be removed altogether 
+const ps = require("prompt-sync");
 const prompt = ps();
 
-// similar to saying printf(); 
+//varables should remained unchanged and must equal something
+//the user input is placeholder code to show that it works
 let currency1 = prompt("Enter your desired currency: ")
 let currency2 = prompt("Enter the currency you want to convert: ");
 var amount = prompt("Enter the amount of currency you want to convert: ");
 
-// Reading the csv file: 
+
+//Highly recommnded not to change code below
+//Code parses the desired data in the csv file depending on the values of currency1 and currency2 variables
+//-start-
 fs.createReadStream('Currency Database.csv')
-  .pipe(csv())  //perfroming functions without having to call so many at once: 
+  .pipe(csv())
   .on('data', (data) => {
     if (data['Currency_Type'] === currency1) {
-      results.push(data); //
+      results.push(data);
     }
-  }) //data = some logic that will check the csv files currency types and compare to the desired currency.. then push the currency on to the data variable
+  })
   .on('end', () => {
     global.number1 = results[0].USD_Rate;
   })
 
 
-fs.createReadStream('Currency Database.csv') //reads fromt the CSV file
+  fs.createReadStream('Currency Database.csv')
   .pipe(csv())
   .on('data', (data) => {
     if (data['Currency_Type'] === currency2) {
@@ -42,33 +44,27 @@ fs.createReadStream('Currency Database.csv') //reads fromt the CSV file
   .on('end', () => {
     global.number2 = results[1].USD_Rate;
   })
+//-end-
 
-const app = express(); //will controll web app processes and allow us to see output. 
+const app = express();
 
-app.get("/", function (req, res) {
+//can be modified to output differently 
+app.get("/", function(req, res) {
 
+  //Code below is the main conversion
+  //modification not recommended
+  //-start-
   var result = (amount * global.number1) / global.number2;
 
   result = Math.floor(result * 100) / 100
+  //-end-
 
   res.send(result + "");
 });
 
-//app will then listen for these values and output 
-app.listen(PORT, function(err){
 
-  if(err){ //if error function is true, meaning we are in a  different port or setup was bad
-    console.log("Error in server setup...");
-  }else{
-  console.log("Server is up and running on port:", PORT); //the ideal output
-  }
+//can be modified/removed 
+//generic code to output to a webpage
+app.listen(5555, function() {
+  console.log(`Server is up and running on 5555 ...`);
 });
-
-// process.once('SIGUSR2', function () {
-//   process.kill(process.pid, 'SIGUSR2');
-// });
-
-// process.on('SIGINT', function () {
-//   // this is only called on ctrl+c, not restart
-//   process.kill(process.pid, 'SIGINT');
-// });
